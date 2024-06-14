494c1a462e239a02ea35324c7c617ba668669201
TubaHorse <sgombas@hotmail.com>
2024-06-14 02:48:34 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,1 @@
+With this mod for Arma 3 I am attempting to turn the default helicopter helmet with the visor to be pink. I have tried so many times but the game will not recognise the .paa that I have set for it to use.
---
+++ pinkHGU/mod.cpp
@@ -0,0 +1,18 @@
+name = "Pink HGU Test";
+picture = "-";
+logoSmall = "-";
+logo = "-";
+logoOver = "-";
+action = "-";
+actionName = "-";
+tooltipOwned = "Pink HGU Test";
+dlcColor[] =
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+overview = "Trying to turn the heli pilot helmet pink";
+hideName = 0;
+author = "TubaHorse";
