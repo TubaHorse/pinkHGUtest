46e1b7b3b8b5c83b442bf6c0c36c3775b6ccb1a2
TubaHorse <sgombas@hotmail.com>
2024-06-15 14:29:45 -0400

    Code Rewrite 1
    Packed version of the files

---
+++ @pinkHGU/mod.cpp
@@ -0,0 +1,18 @@
+name = "Pink HGU";
+picture = "-";
+logoSmall = "cagemblem.paa";
+logo = "cagemblem.paa";
+logoOver = "cagemblem.paa";
+action = "-";
+actionName = "-";
+tooltipOwned = "Pink HGU";
+dlcColor[] =
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+overview = "Makes the HGU pink";
+hideName = 0;
+author = "The CAG";
